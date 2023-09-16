306e684b0ecc27d65ebd310dc5c90b26c2c9a3a0
Viper_zero_zero <taylor.j.nastally@gmail.com>
2023-09-15 19:45:13 -0700

    basic UI for application run troubleshooting

--- CrossTok.py
+++ CrossTok.py
@@ -1,1 +1,10 @@
-
+print("******************* Welcome to CrossTok *****************\n")
+print("In this application you can do the following:\n")
+print("MYIP: returns your IPv4 address\n")
+print("HELP: returns commands you can type to use the program\n")
+print("MYPORT: returns the port CrossTok is using to listen on\n")
+print("CONNECT <dest IP> <port #>: establishes TCP connection to the <dest IP> on <port #>\n")
+print("LIST: Display a numbered list of all connections CrossTok is using with an id number\n")
+print("TERMINATE: <connection id>: close connection denoted with <connection id>\n")
+print("SEND: <connection id> <message>: send a message to <connection id> with string <message>\n")
+print("EXIT: terminate CrossTok and all active connections\n")
