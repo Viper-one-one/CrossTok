14c06f9191962219ba3c9cdc9484c5562d6f7306
Viper-one-one <taylor.j.nastally@gmail.com>
2023-11-01 01:49:23 -0700

    halfway implementation change

--- CrossTok.py
+++ CrossTok.py
@@ -1,33 +1,37 @@
 from asyncio.windows_events import NULL
 import socket
 import sys
+import os
 import threading
 
 #TODO: implement a server socket and a client socket
 #TODO: server socket will bind, listen, accept, loop thru recv, send; close
 #TODO: client socket will connect, loop thru send, recv; close
-#
-#
-#
 
 host = socket.gethostbyname(socket.gethostname())
 port = 5959
 max_conn = 10
+buffer_size = 1024
 connections = []    #stored as (IP, Port) pairs
+clients_list = []   #contains sockets as items
 
-if sys.argv.__sizeof__() == 1:
-    port = sys.argv[1]
-else:
-    pass
-
-#server function
-#needs to listen on port for new connections, add to connection list when new connection spawned
-def server_func(server, host, port):
-    while True:
-        connections.append((host, port))
-        ip_address, port = server.accept()
+if len(sys.argv) == 2:
+    try:
+        port = int(sys.argv[1])
+    except ValueError:
+        print(f"Bad value provided, defaulting to: {port}")
+elif (len(sys.argv) > 2):
+    print("you have used too many system arguments \n using program defaults")
         
+#we'll use this socket to listen
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.bind((host, port))
+client_socket.listen(5)
 
+
+#we'll use this socket to send messages out
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
 def print_help():
     print("******************* Welcome to CrossTok *****************\n")
     print("In this application you can do the following:")
@@ -38,27 +42,43 @@ def print_help():
     print("LIST: Display a numbered list of all connections CrossTok is using with an id number")
     print("TERMINATE: <connection id>: close connection denoted with <connection id>")
     print("SEND: <connection id> <message>: send a message to <connection id> with string <message>")
+    print("CLS: clears the current screen")
+    print("SOCKET: recreates the socket if a fatal error occured")
     print("EXIT: terminate CrossTok and all active connections\n")
-    print("\n")    
+    print("\n")  
     
-def incoming_message_handler(target_socket, sender_addr, sender_port):
+def recieve_connections():
     while True:
         try:
-            message = target_socket.recv(2048).decode('utf-8')
-            if message == NULL:
-                print(f"User {sender_addr} on port {sender_port} disconnected")
-                break
-            print(f"Message from {sender_addr}")
-            print(f"Sender's Port: {sender_port}")
-            print(f"Message: \"{message}\"")
-        except Exception as e:
-            print(f"Exception {e} caught")
-            break
-    target_socket.close()
-    connections.remove((sender_addr, sender_port))
+            client, address = client_socket.accept()
+            connections.append((client.getpeername(socket.gethostname()), port))
+            clients_list.append(client)
+        except 
     
-def client_func():
-    return 0
+def receive_messages():
+    while True:
+        try:
+            message = client_socket.recv(buffer_size).decode()
+            if message.startswith("EXIT"):                          #if user disconnected
+                remove_id = client_socket.recvfrom(buffer_size)     #(ip, port) tuple
+                try:
+                    connections.remove(remove_id)
+                except ValueError:
+                    print("a user tried to disconnect who did not exist in the connections list")
+            else:
+                print(message)
+        except Exception as e:
+            print(f"Error: {e}")
+            print("socket must close, you will have to recreate the socket")
+            client_socket.close()
+            break
+   
+thread = threading.Thread(target=receive_messages)
+thread.start()
+
+def send_message(message):
+    # encode and send the message
+    client_socket.send(message.encode())
 
 # TODO: make connect and list work
 # TODO: list should find all unique connected clients, store conn list in list
@@ -66,19 +86,15 @@ def client_func():
 # TODO: connect should ping the target ip with TCP connection request which triggers addition to the connected clients list
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    server.listen(max_conn)
-    threading.Thread(target=server_func, args=(server,host, port)).start()
+    clear = lambda: os.system('cls' if os.name == 'nt' else clear)
+    print_help()
     
     while True: 
         
-        print_help()
         print("CMD: ", end='')  
         user_choice = input()
         user_choice = user_choice.split()
         user_choice[0] = user_choice[0].upper()
-        
         
         if (user_choice[0] == "MYIP"):
             print("Your IP Address: " + socket.gethostbyname(socket.gethostname()) + "\n")
@@ -87,16 +103,27 @@ def main():
         elif (user_choice[0] == "HELP"):
             print_help()
         elif (user_choice[0] == "CONNECT"):
-            target_ip = user_choice[1]
-            target_port = int(user_choice[2])
-            try:
-                target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                target_socket.connect((target_ip, target_port))                         #failed due to str cannot be interpreted as integer
-                connections.append((target_ip, target_port))
-                print(f"Successfully Connected to : {target_ip}:{target_port}")
-                threading.Thread(target=incoming_message_handler, args=(target_socket, target_ip, target_port)).start()
-            except Exception as e:
-                print(f"Failed due to: {e}")
+            if (len(user_choice) == 3):
+                target_ip = user_choice[1]
+                try:
+                    target_port = int(user_choice[2])
+                    connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    connection_socket.connect((target_ip, target_port))                         #failed due to str cannot be interpreted as integer
+                    connection_socket.send("connection attempt")
+                    connections.append((target_ip, target_port))
+                    print(f"Successfully Connected to : {target_ip}:{target_port}")
+                except ValueError:
+                    print("please ensure you are using the correct format for ip and port")
+                except socket.gaierror:
+                    print(f"invalid address or hostname: {target_ip} {target_port}")
+                except socket.timeout:
+                    print("connection timed out")
+                except ConnectionRefusedError:
+                    print("the user you targeted refused your connection")
+                except Exception as e:
+                    print(f"Unknown Failure")
+            else:
+                print("\nplease see the help menu for information on how to use the connect command")
         elif (user_choice[0] == "LIST"):
             print("Connection list: ")
             for i, (conn_ip, conn_port) in enumerate(connections, start=0):
@@ -118,23 +145,25 @@ def main():
                 conn_ip.send(message)
             except Exception as e:
                 print("Uh-oh, looks like something went wrong!\nCheck the id you entered and try again!")
+        elif (user_choice[0] == "CLS"):
+            clear()
+        elif (user_choice[0] == "SOCKET"):
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.bind((host, port))
+            client_socket.listen()
         elif (user_choice[0] == "EXIT"):
-            for conn_ip, conn_port in connections:
-                try:
-                    target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    target.connect((conn_ip, conn_port))
-                    target.send('exit'.encode('utf-8'))
-                    target.close()
-                    print(f"Connection to {conn_ip}:{conn_port} closed.")
-                except:
-                    pass
+            try:
+                client_socket.sendall("EXIT".encode())
+                client_socket.close()
+                for connection in connections:
+                    connections.remove()
+                print(f"Connections closed.")
+            except:
+                print("nothing connected")
             print("Sorry to see you go!")
             quit()
-            break
         else:
             print("\n\nERROR: invalid entry\n\n")
 
 if __name__ == "__main__":
     main()
-
-
